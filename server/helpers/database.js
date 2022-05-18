91f095ee6866209fff96af40bc00b26bcdd9889f
thitiph0n <thitiphon.ks@gmail.com>
2022-05-19 04:16:46 +0700

    feat: change db password

--- server/helpers/database.js
+++ server/helpers/database.js
@@ -1,26 +1,26 @@
-if (!process.env.NODE_ENV) require("dotenv").config();
+if (!process.env.NODE_ENV) require('dotenv').config();
 
-const util = require("util");
-const mysql = require("mysql");
+const util = require('util');
+const mysql = require('mysql');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
+  password: 'banana',
   database: process.env.DATABASE_NAME,
 });
 
 pool.getConnection((err, connection) => {
   if (err) {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.error("Database connection was closed.");
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.');
     }
-    if (err.code === "ER_CON_COUNT_ERROR") {
-      console.error("Database has too many connections.");
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.');
     }
-    if (err.code === "ECONNREFUSED") {
-      console.error("Database connection was refused.");
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.');
     }
   }
 
